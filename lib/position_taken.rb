84bf5876adc12d815074f8edadcab697c5d13307
Rickie Dixon <rhdixon79@gmail.com>
2020-11-25 01:30:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,8 +6,8 @@ return(false)
  elsif (board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] || index == 0 || index == 1)
  return(false)
 
- elsif(board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] || index == 0 || index == 8)
-  return(true)
+ #elsif(board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] || index == 0 || index == 8)
+  #return(true)
 
 elsif(board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] || index == 0 || index == 8)
   return(false)
